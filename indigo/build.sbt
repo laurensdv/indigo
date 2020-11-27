b85a4fe755d8f66b7688f8b4b133e3d898fe9202
laurensdv <laurensdv@gmail.com>
2020-11-27 15:46:34 +0000

    put back the generic circe parser

--- indigo/build.sbt
+++ indigo/build.sbt
@@ -187,6 +187,7 @@ lazy val indigoJsonCirce =
       name := "indigo-json-circe",
       libraryDependencies ++= Seq(
         "io.circe" %%% "circe-core",
+	"io.circe" %%% "circe-generic",
         "io.circe" %%% "circe-parser"
       ).map(_ % "0.13.0")
     )
--- indigo/indigo-json-circe/src/main/scala/indigo/json/Json.scala
+++ indigo/indigo-json-circe/src/main/scala/indigo/json/Json.scala
@@ -4,19 +4,15 @@ import indigo.shared.JsonSupportFunctions
 import indigo.shared.formats.{Aseprite, TiledMap}
 import indigo.shared.IndigoLogger
 
+import io.circe.generic.auto._
 import io.circe.parser._
 import indigo.shared.datatypes.FontChar
-import indigo.json.core.GlyphWrapper
 
 object Json extends JsonSupportFunctions {
 
-  import core.CirceJsonEncodersAndDecoders._
-
   def asepriteFromJson(json: String): Option[Aseprite] =
     decode[Aseprite](json) match {
-      case Right(s) =>
-        Some(s)
-
+      case Right(s) => Some(s)
       case Left(e) =>
         IndigoLogger.info("Failed to deserialise json into Aseprite: " + e.getMessage)
         None
@@ -24,9 +20,7 @@ object Json extends JsonSupportFunctions {
 
   def tiledMapFromJson(json: String): Option[TiledMap] =
     decode[TiledMap](json) match {
-      case Right(s) =>
-        Some(s)
-
+      case Right(s) => Some(s)
       case Left(e) =>
         IndigoLogger.info("Failed to deserialise json into TiledMap: " + e.getMessage)
         None
@@ -34,12 +28,15 @@ object Json extends JsonSupportFunctions {
 
   def readFontToolJson(json: String): Option[List[FontChar]] =
     decode[GlyphWrapper](json) match {
-      case Right(s) =>
-        Some(s.glyphs.map(_.toFontChar))
-
+      case Right(s) => Some(s.glyphs.map(_.toFontChar))
       case Left(e) =>
         IndigoLogger.info("Failed to deserialise json into a list of glyphs: " + e.getMessage)
         None
     }
 
+  private final case class GlyphWrapper(glyphs: List[Glyph])
+  private final case class Glyph(char: String, x: Int, y: Int, w: Int, h: Int) {
+    def toFontChar: FontChar =
+      FontChar(char, x, y, w, h)
+  }
 }
